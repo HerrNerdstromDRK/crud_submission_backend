71f8945ff4610f53742e972d92a96c771c4ea5fa
HerrNerdstromDRK <coding@karrels.com>
2023-01-20 19:19:35 -0600

    Updated CORS

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,7 @@
-const whitelist = [
+const allowedOrigins = [
   // TODO    'https://www.yoursite.com',
   "http://127.0.0.1:3000",
+  "https://karrels-crud-submission.onrender.com",
   "http://localhost:3000",
   "http://127.0.0.1:4000",
   "http://localhost:4000",
@@ -15,7 +16,7 @@ const whitelist = [
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
